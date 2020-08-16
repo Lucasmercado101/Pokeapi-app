a53a4ab718b348ab5b7a710e7bbbd9800ca0e638
Lucas <lucasmercado101@gmail.com>
2020-08-16 15:47:46 -0300

    Updates

---
+++ precache-manifest.bb0afe71b485849ef9412168b30ba1ec.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebcf062da784d60a11c811d0e3c8c2d2",
+    "url": "/Pokeapi-app/index.html"
+  },
+  {
+    "revision": "6f88162fd908800d8a84",
+    "url": "/Pokeapi-app/static/css/main.d322556e.chunk.css"
+  },
+  {
+    "revision": "913cf1ae50b8fc9f533d",
+    "url": "/Pokeapi-app/static/js/2.a8b827cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Pokeapi-app/static/js/2.a8b827cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f88162fd908800d8a84",
+    "url": "/Pokeapi-app/static/js/main.e6317fd8.chunk.js"
+  },
+  {
+    "revision": "bb58993a811af7da5edf",
+    "url": "/Pokeapi-app/static/js/runtime-main.f5b1a53f.js"
+  },
+  {
+    "revision": "7da2026c8e75926cbe8e041b1306c75d",
+    "url": "/Pokeapi-app/static/media/arrow.7da2026c.svg"
+  },
+  {
+    "revision": "a5e21982d078314b325fd6c6c6ced6af",
+    "url": "/Pokeapi-app/static/media/grape.a5e21982.svg"
+  },
+  {
+    "revision": "a7662a6306d4ecb1bfb5496e4cce0f85",
+    "url": "/Pokeapi-app/static/media/pikachuface.a7662a63.svg"
+  },
+  {
+    "revision": "36d18d8bde83c043c40784e85fa1e7b5",
+    "url": "/Pokeapi-app/static/media/planet.36d18d8b.svg"
+  },
+  {
+    "revision": "6b4ffa55fdd7e087c6990685887572b4",
+    "url": "/Pokeapi-app/static/media/pokeball.6b4ffa55.svg"
+  },
+  {
+    "revision": "3cd8358d503ce34552077bddab1a680c",
+    "url": "/Pokeapi-app/static/media/search.3cd8358d.svg"
+  },
+  {
+    "revision": "168b3cc5213c5ec57ffbf1e7cc908055",
+    "url": "/Pokeapi-app/static/media/shoppingBag.168b3cc5.svg"
+  }
+]);
